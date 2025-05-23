88bc2da428e888463c0b36c97c2b4ecfab37b85c
kanes <kanes.srichum@gmail.com>
2025-05-23 11:57:05 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  assetPrefix: './',
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;
