24f63a596b6f1f01575065279ac0c4f3380a56f1
kanes <kanes.srichum@gmail.com>
2025-05-23 12:03:03 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// 📁 pages/index.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login'); // redirect ไป /login
+  }, [router]);
+
+  return null;
+}
